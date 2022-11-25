6ffc96b97ce458d3f0156bf311fb101c683b4b71
Kanyapatchpor <aipokanyapatch@gmail.com>
2022-11-25 23:14:14 +0700

    first commit

---
+++ src/Element/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import { Nav } from "react-bootstrap";
+//import { Link } from "react-router-dom";
+//import Navbar from "./components/NavbarComp";
+const Home = () => {
+  return (
+    <div className="App">
+      <h1>Myname is Kanyapatch konnin </h1>
+      <p> 6404062620125</p>
+    </div>
+  );
+};
+export default Home;
